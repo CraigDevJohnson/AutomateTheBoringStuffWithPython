80f3d700ee86ea59496e9786a8379076344b93a5
Craig Johnson <craigdevjohnson@gmail.com>
2021-02-02 17:52:43 -0900

    Added second example script

---
+++ if_example.py
@@ -0,0 +1,31 @@
+# if examples
+
+name = 'Alice'
+if name == 'Alice':
+    print('Hi, ' + name + '!')
+else:
+    print('Oh, I was expecting Alice... Still nice to met you ' + name + '!')
+print('Done')
+
+name = 'Bob'
+if name == 'Alice':
+    print('Hi, ' + name + '!')
+else:
+    print('Well, hello there ' + name + '!')
+print('Done')
+
+name = 'Alice'
+if name == 'Alice':
+    print('Hi, ' + name + '!')
+else:
+    print('Oh, I was expecting Alice... Still nice to met you ' + name + '!')
+print('Done')
+
+name = 'Bob'
+if name == 'Alice':
+    print('Hi, ' + name + '!')
+elif name == 'Bob':
+    print('Hi there, ' + name + '!')
+else:
+    print('Well, hello there ' + name + '!')
+print('Done')
