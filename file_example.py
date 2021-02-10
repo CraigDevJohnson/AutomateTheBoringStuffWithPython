e5d97852430197493270768a34b08d87f2025eff
Craig Johnson <craigdevjohnson@gmail.com>
2021-02-10 09:37:58 -0900

    Changed path so it will work on Windows or Linux

--- file_example.py
+++ file_example.py
@@ -1,7 +1,7 @@
 import os
 totalSize = 0
-for filename in os.listdir('/home/cjohnson'):
-    if not os.path.isfile(os.path.join('/home/cjohnson', filename)):
+for filename in os.listdir(os.getcwd()):
+    if not os.path.isfile(os.path.join(os.getcwd(), filename)):
         continue
-    totalSize = totalSize + os.path.getsize(os.path.join('/home/cjohnson', filename))
+    totalSize = totalSize + os.path.getsize(os.path.join(os.getcwd(), filename))
 print(totalSize)
